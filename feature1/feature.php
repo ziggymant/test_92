88a4a97f4052698f9117aec0342f933767a3404b
Ziggy <z.makunas@gmail.com>
2018-09-20 17:56:58 +0200

    Added feature1 and added sth files

---
+++ feature1/feature.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 20.09.2018
+ * Time: 17:52
+ */
+class feature1 {
+    const NAME = "Feature name1";
+    public function doSth(){
+        echo self::NAME;
+    }
+}
